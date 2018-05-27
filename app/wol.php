eb092e7794c505a8a7fbae3df7089500aff8a675
projectflx <dev@projectflx>
2018-05-27 20:19:57 +0200

    Using socket_create instead of fsockopen

--- app/wol.php
+++ app/wol.php
@@ -5,25 +5,34 @@
     */
     function WakeOnLan($mac_address) {
         // Check if the MAC address is valid
-        if(preg_match('/^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$/',$mac_address)) {
-            // Remove unwanted chars and convert to binary
-            $binaryMAC = pack('H12', str_replace(':','', $mac_address));
+        if(extension_loaded('sockets')) {
+            if(preg_match('/^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$/',$mac_address)) {
+                // Remove unwanted chars and convert to binary
+                $binaryMAC = pack('H12', str_replace(':','', $mac_address));
 
-            // Build packet
-            $magicPacket = str_repeat(chr(0xff), 6).str_repeat($binaryMAC, 16);
+                // Build packet
+                $magicPacket = str_repeat(chr(0xff), 6).str_repeat($binaryMAC, 16);
 
-            if (!$socket = fsockopen('udp://255.255.255.255', 40000, $errno, $errstr, 2)) {
-                // Opening the UDP socket was not successful
-                return 'Opening the UDP socket was not successful';
-             }
+                //if (!$socket = fsockopen('udp://255.255.255.255', 40000, $errno, $errstr, 2)) {
+                if (!$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) {
+                    // Opening the UDP socket was not successful
+                    return 'Opening the UDP socket was not successful: Error ' . socket_last_error($socket) . '. ' . socket_strerror(socket_last_error($socket));
+                }
 
-             // Write packet to the stream and close it
-             fputs($socket, $magicPacket);
-             fclose($socket);
-             return 0;
+                if($socket_options = socket_set_option($socket, SOL_SOCKET, SO_BROADCAST, 1) < 0) {
+                    return 'Setting the socket options failed. ' . socket_strerror($socket_options);
+                }
+
+                if(socket_sendto($socket, $magicPacket, strlen($magicPacket), 0, '255.255.255.255', 40000) == -1) {
+                    return 'Error while sending the packet.';
+                }
+                return 0;
+            } else {
+                // error code for an invalid MAC
+                return 'The MAC address entered is invalid.';
+            }
         } else {
-            // error code for an invalid MAC
-            return 'The MAC address entered is invalid.';
+            return 'Needed extension \'sockets\' is not enabled.';
         }
     }
 
