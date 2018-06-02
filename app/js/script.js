e0b041a7d0e4ccd982cae0e41e573669b995a950
projectflx <dev@projectflx>
2018-06-02 02:32:37 +0200

    Error messages behavior and JS clean-up

--- app/js/script.js
+++ app/js/script.js
@@ -7,15 +7,41 @@ function wakeUpWithWOL(host, mac) {
         },
         success: function(data) {
             if(data == 0) {
-                showNotifyBox('Magic Packet send to '.concat(host).concat('.'), 'success');
+                showNotifyBox('Magic Packet send to '.concat(host).concat('.'));
             } else {
-                showNotifyBox( data, 'danger');
+                showErrorNotifyBox(data);
             }
         }
     });
 }
 
-function showNotifyBox(message, type) {
+function showNotifyBox(message) {
+    $.notify({
+        // options
+        message: message
+    },{
+        // settings
+        position: null,
+        placement: {
+            from:   'top',
+            align:  'center'
+        },
+        offset: 0,
+        spacing: 0,
+        delay: 3000,
+        url_target: '_blank',
+        type:       'success',
+        animate: {
+            enter:  'animated fadeInDown',
+            exit:   'animated fadeOutUp'
+        },
+        template:   '<div data-notify="container" class="col-xs-11 alert alert-{0}" role="alert">' +
+                        '<span data-notify="message">{2}</span>' +
+                    '</div>'
+    });
+}
+
+function showErrorNotifyBox(message) {
     $.notify({
         // options
         message: message
@@ -24,21 +50,21 @@ function showNotifyBox(message, type) {
         allow_dismiss: true,
         position: null,
         placement: {
-            from: 'top',
-            align: 'center'
+            from:   'top',
+            align:  'center'
         },
         offset: 0,
         spacing: 0,
-        delay: 1000,
-        timer: 1000,
+        delay: 0,
         url_target: '_blank',
-        type: type,
+        type:       'danger',
         animate: {
-            enter: 'animated fadeInDown',
-            exit: 'animated fadeOutUp'
+            enter:  'animated fadeInDown',
+            exit:   'animated fadeOutUp'
         },
-        template: '<div data-notify="container" class="col-xs-11 alert alert-{0}" role="alert">' +
-                '<span data-notify="message">{2}</span>' +
-            '</div>'
+        template:   '<div data-notify="container" class="col-xs-11 alert alert-{0}" role="alert">' +
+                        '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+                        '<span data-notify="message">{2}</span>' +
+                    '</div>'
     });
 }
